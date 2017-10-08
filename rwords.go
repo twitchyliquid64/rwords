67f37efd6c033f378f03c8365ef57787c19a649e
Tom <twitchyliquid64@ciphersink.net>
2017-10-08 16:54:52 +1100

    Implemented random generators

--- rwords.go
+++ rwords.go
@@ -1,8 +1,15 @@
 package rwords
 
 import (
+	"math/rand"
 	"sort"
 	"strings"
+	"time"
+)
+
+var (
+	vowels = []string{"a", "e", "i", "o", "u"}
+	consts = []string{"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "qu", "r", "s", "t", "v", "w", "x", "y", "z", "tt", "ch", "sh"}
 )
 
 func buildMap(input string) map[byte][]wordWeight {
@@ -44,6 +51,54 @@ func sortMap(in map[byte][]wordWeight) map[byte][]wordWeight {
 		s := v
 		sort.Sort(ByWeight(s))
 		out[k] = s
+	}
+	return out
+}
+
+// RandomMarkov generates a word based on the markov distribution of letters in the space-delimited training set.
+func RandomMarkov(training string) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	m := sortMap(buildMap(training))
+	var cursor byte
+	var out string
+
+	for k, _ := range m {
+		cursor = k
+		break
+	}
+
+	out += string(cursor)
+	for {
+		choice := weightedChoice(m[cursor], r)
+		if len(choice.word) == 0 {
+			break
+		}
+		cursor = byte(choice.word[0])
+		out += choice.word
+	}
+	return out
+}
+
+// RandomSimple returns a random pronouncable word with the given number of sound components.
+func RandomSimple(sounds int) string {
+	return randomSimple(sounds, rand.New(rand.NewSource(time.Now().UnixNano())))
+}
+
+func randomSimple(sounds int, rand *rand.Rand) string {
+	isVowel := false
+	out := ""
+
+	for i := 0; i < sounds; i++ {
+		var source []string
+		if isVowel {
+			source = vowels
+		} else {
+			source = consts
+		}
+
+		isVowel = !isVowel
+
+		out += source[rand.Intn(len(source)-1)]
 	}
 	return out
 }
--- rwords_test.go
+++ rwords_test.go
@@ -1,6 +1,9 @@
 package rwords
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 var testStr = "the quick brown fox jumped over the lazy dog ExpFloat64 returns an exponentially distributed float64 in the range. default Source is safe for concurrent use by multiple goroutines"
 
@@ -46,5 +49,43 @@ func TestSortMap(t *testing.T) {
 	// check order
 	if len(m['e']) != 8 || m['e'][7].word != "" {
 		t.Errorf("Expected 8 options for e (%d) ending in '', instead got %+v", 'e', m['e'])
+	}
+}
+
+func TestRandomSimple(t *testing.T) {
+	type simpleWordsCase struct {
+		Seed       int64
+		Expected   string
+		Components int
+	}
+
+	cases := []simpleWordsCase{
+		simpleWordsCase{
+			Seed:       4657543,
+			Expected:   "wavez",
+			Components: 5,
+		},
+		simpleWordsCase{
+			Seed:       1,
+			Expected:   "kowor",
+			Components: 5,
+		},
+		simpleWordsCase{
+			Seed:       9,
+			Expected:   "naj",
+			Components: 3,
+		},
+		simpleWordsCase{
+			Seed:       9432345,
+			Expected:   "bilechi",
+			Components: 6,
+		},
+	}
+	for i, c := range cases {
+		r := rand.New(rand.NewSource(c.Seed))
+		w := randomSimple(c.Components, r)
+		if w != c.Expected {
+			t.Errorf("[%d] Got %q, expected %q", i, w, c.Expected)
+		}
 	}
 }
